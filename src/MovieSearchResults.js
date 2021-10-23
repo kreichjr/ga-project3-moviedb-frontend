3e9fad577323b0ca988fa72992b9a26842c665af
jessielista <jmgonzalez8282@gmail.com>
2021-10-23 12:32:06 -0700

    api works

--- src/MovieSearchResults.js
+++ src/MovieSearchResults.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import './App.css'
 
 class MovieSearchResults extends Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
   render() {
     return (
       <div className='App'>
@@ -10,9 +15,19 @@ class MovieSearchResults extends Component {
           src='https://media.istockphoto.com/vectors/movie-time-vector-illustration-cinema-poster-concept-on-red-round-vector-id911590226?k=20&m=911590226&s=612x612&w=0&h=HlJtSKF-fLsKFy1QJ-EVnxXkktBKNS-3jUQPXsSasYs='
           alt='movie film and popcorn'
         />
-        <p>Search a film by Title or IMDB Number</p>
-        <input type="text" placeholder="Title or IMDB"></input>
-        <input type="button" value="Search"></input>
+        <p>Search a film by Title</p>
+        <input
+          type='text'
+          id='titleSearchField'
+          placeholder='Title'
+          onChange={this.props.handleChange}
+          value={this.props.title}
+        ></input>
+        <input
+          type='button'
+          value='Search'
+          onClick={this.props.handleSubmit}
+        ></input>
       </div>
     )
   }
