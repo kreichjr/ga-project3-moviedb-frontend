aae51b41364a4f99f88958a4984fa72c568215dd
jessielista <jmgonzalez8282@gmail.com>
2021-10-23 14:46:59 -0700

    movie posters appear

---
+++ src/MovieSearchDisplay.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+// pass in the array movielist as the prop
+
+const MovieSearchDisplay = (props) => {
+  // console.log(props)
+  return (
+    <>
+      {props.movieList.map((movie, index) => (
+        <div className='' key={index}>
+          {(movie.Poster === 'N/A' && 'poster missing' )|| <img src={movie.Poster} alt ="movie"/>}
+        </div>
+      ))}
+    </>
+  )
+}
+
+
+export default MovieSearchDisplay
