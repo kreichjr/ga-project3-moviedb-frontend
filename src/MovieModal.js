a9440e5f0a099105db4f540dc71cce4c3c85c392
kreichjr <kreichjr@gmail.com>
2021-10-26 21:13:46 -0500

    Updated modal to display on click of picture

--- src/MovieModal.js
+++ src/MovieModal.js
@@ -3,20 +3,25 @@
 // The variable showHideClassName assigns its value a conditional to check if the value of the show property in your state is true. If so, the modal will appear. Else, the modal will hide. The properties display-block and display-none to show and hide the modal are handled through the modal.css file imported into your Modal component.
 import React from "react";
 import './ModalStyling.css';
+import missing from './images/imgNotFound.jpg'
 
 const MovieModal = ({ handleClose, show, children, movie }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
   const movieInfo = []
   Object.keys(movie).forEach((key) => {
-    if (key != 'Poster' && key != 'Response') {
+    if (key !== 'Poster' && key !== 'Response') {
       movieInfo.push(`${key}: ${movie[key]}`)
     }
   })
+  console.log(show)
   
   return (
     <div className={showHideClassName}>
       <section className="modal-view">
-        <img src={movie.Poster} alt='movie poster'/>
+        {
+            (movie.Poster === 'N/A' && <img src={missing} alt='movie poster missing'/> ) || 
+            <img src={movie.Poster} alt="movie"/>
+        }
         <ul>
           {movieInfo.map((info, i)=><li key={i}>{info}</li>)} 
         </ul>
