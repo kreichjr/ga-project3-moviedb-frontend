6f0b77fc8587b4de88f804fd67f8377d6de5a929
jessielista <jmgonzalez8282@gmail.com>
2021-10-27 19:13:34 -0700

    search bar

--- src/MovieModal.js
+++ src/MovieModal.js
@@ -1,12 +1,12 @@
 // Your return() statement passes the argument children, represented as props.children, is a reference to the functionality of opening and closing the modal. The modal also contains a button with a the React JSX onClick attribute that accepts the hideModal() method, here represented as the argument handleClose, passed as props into your Dashboard component.
 
 // The variable showHideClassName assigns its value a conditional to check if the value of the show property in your state is true. If so, the modal will appear. Else, the modal will hide. The properties display-block and display-none to show and hide the modal are handled through the modal.css file imported into your Modal component.
-import React from "react";
-import './ModalStyling.css';
+import React from 'react'
+import './ModalStyling.css'
 import missing from './images/imgNotFound.jpg'
 
 const MovieModal = ({ handleClose, show, children, movie, addToFavs }) => {
-  const showHideClassName = show ? "modal display-block" : "modal display-none";
+  const showHideClassName = show ? 'modal display-block' : 'modal display-none'
   const movieInfo = []
   const blacklist = [
     'Poster',
@@ -34,21 +34,30 @@ const MovieModal = ({ handleClose, show, children, movie, addToFavs }) => {
 
   return (
     <div className={showHideClassName}>
-      <section className="modal-view">
-        {
-            (movie.Poster === 'N/A' && <img src={missing} alt='movie poster missing'/> ) || 
-            <img src={movie.Poster} alt="movie"/>
-        }
+      <section className='modal-view'>
+        {(movie.Poster === 'N/A' && (
+          <img src={missing} alt='movie poster missing' />
+        )) || (
+          <img className='movieModal-poster' src={movie.Poster} alt='movie' />
+        )}
 
         <ul>
-          {movieInfo.map((info, i)=><li key={i}>{info}</li>)} 
+          {movieInfo.map((info, i) => (
+            <li key={i}>{info}</li>
+          ))}
         </ul>
-        <button onClick={() => {addToFavs(movie)}}>Add To Favorites</button>
-        <button type="button" onClick={handleClose}>
+        <button
+          onClick={() => {
+            addToFavs(movie)
+          }}
+        >
+          Add To Favorites
+        </button>
+        <button type='button' onClick={handleClose}>
           Close
         </button>
       </section>
     </div>
-  );
-};
+  )
+}
 export default MovieModal
--- src/MovieSearchResults.js
+++ src/MovieSearchResults.js
@@ -11,21 +11,23 @@ class MovieSearchResults extends Component {
   render() {
     return (
       <>
-        <p></p>
-        <input
-          className='inputText'
-          type='text'
-          id='titleSearchField'
-          placeholder='Enter Movie Title...'
-          onChange={this.props.handleChange}
-          value={this.props.title}
-        ></input>
-        <input
-          className='inputBtn'
-          type='button'
-          value='Search'
-          onClick={this.props.handleSubmit}
-        ></input>
+        <div className='searchAndBtnRow'>
+          <p></p>
+          <input
+            className='inputText'
+            type='text'
+            id='titleSearchField'
+            placeholder='Enter Movie Title...'
+            onChange={this.props.handleChange}
+            value={this.props.title}
+          ></input>
+          <input
+            className='inputBtn'
+            type='button'
+            value='Search'
+            onClick={this.props.handleSubmit}
+          ></input>
+        </div>
       </>
     )
   }
